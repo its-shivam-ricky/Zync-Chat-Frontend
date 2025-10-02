23818acd75632bd1bafb4d208714e3717fbc93ce
Shivam Vishwakarma <shivam.ricky1999@gmail.com>
2025-10-02 18:56:15 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5001/api" 
-  : "https://my-backend.onrender.com/api";
+  : "https://zync-chat-backend.onrender.com/";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
